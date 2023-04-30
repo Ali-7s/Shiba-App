cb0cb35d2470fb7a19616830d0bd09cec13dbae6
Ali Sharif <alisharif29@gmail.com>
2023-04-29 22:29:34 -0700

    Initial commit

--- app/src/main/java/com/example/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -1,30 +1,43 @@
 package com.example.myfirstapp
 
 import android.os.Bundle
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
+import androidx.core.view.isVisible
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val firstButton: TextView = findViewById(R.id.button1)
         val secondButton: TextView = findViewById(R.id.button2)
-        val numberTextViewer: TextView = findViewById(R.id.numberTextView)
+
         val duration = Toast.LENGTH_SHORT
         val toast = Toast.makeText(applicationContext, "", duration)
+        val imageView: ImageView = findViewById(R.id.imageView)
 
-        numberTextViewer.setTextColor(ContextCompat.getColor(this, android.R.color.white))
-        numberTextViewer.setBackgroundColor(ContextCompat.getColor(this, android.R.color.holo_purple))
-        secondButton.text = "TOASTY"
+        imageView.isVisible = false
 
+        firstButton.text = "Feed"
+        secondButton.text = "Pet"
+
+
+        firstButton.setOnClickListener {
+            imageView.setImageResource(R.drawable.image_3)
+            imageView.isVisible = true
+            toast.setText("very calm...")
+            toast.show()
+
+        }
 
         secondButton.setOnClickListener {
-
-            numberTextViewer.text = "BIG TOAST"
-            toast.setText("ITS TOASTY")
+            imageView.setImageResource(R.drawable.angry_shiba)
+            imageView.isVisible = true
+            toast.setText("GRRRRRRRR")
             toast.show()
         }
     }
