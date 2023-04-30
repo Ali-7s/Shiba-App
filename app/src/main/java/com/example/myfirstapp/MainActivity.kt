3917bdc5cb5367f03db9fec700e39277d8b0293f
Ali Sharif <alisharif29@gmail.com>
2023-04-29 17:18:47 -0700

    Initial commit

--- app/src/main/java/com/example/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -11,33 +11,20 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val firstButton: TextView = findViewById(R.id.button1)
         val secondButton: TextView = findViewById(R.id.button2)
         val numberTextViewer: TextView = findViewById(R.id.numberTextView)
         val duration = Toast.LENGTH_SHORT
         val toast = Toast.makeText(applicationContext, "", duration)
-        var count = 0
 
-        numberTextViewer.text = count.toString()
-        numberTextViewer.setTextColor(ContextCompat.getColor(this, android.R.color.holo_red_dark))
-        numberTextViewer.setBackgroundColor(ContextCompat.getColor(this, android.R.color.black))
-        firstButton.text = "Increment"
-        secondButton.text = "Decrement"
+        numberTextViewer.setTextColor(ContextCompat.getColor(this, android.R.color.white))
+        numberTextViewer.setBackgroundColor(ContextCompat.getColor(this, android.R.color.holo_purple))
+        secondButton.text = "TOASTY"
 
-
-
-
-        firstButton.setOnClickListener {
-            count += 1
-            numberTextViewer.text = count.toString()
-            toast.setText("Incremented!")
-            toast.show()
-        }
 
         secondButton.setOnClickListener {
-            count -= 1
-            numberTextViewer.text = count.toString()
-            toast.setText("Decremented!")
+
+            numberTextViewer.text = "BIG TOAST"
+            toast.setText("ITS TOASTY")
             toast.show()
         }
     }
